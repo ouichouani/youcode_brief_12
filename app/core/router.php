b56318f8b972d0e08afb762d1450442d2373284b
mo24ali <alichere1234@gmail.com>
2026-01-21 20:53:58 +0100

    modified the router + the Router file name

--- app/core/router.php
+++ app/core/router.php
@@ -6,7 +6,6 @@ use App\core\Request;
 
 class Router
 {
-
     private $routes = [
         'get' => [],
         'post' => []
@@ -21,55 +20,72 @@ class Router
 
     public function get(string $path, $callback)
     {
-        $this->routes['get'][$path] = $callback;
+        $this->routes['get'][$this->clean($path)] = $callback;
     }
 
     public function post(string $path, $callback)
     {
-        $this->routes['post'][$path] = $callback;
+        $this->routes['post'][$this->clean($path)] = $callback;
     }
 
-    public function resolve()
+    public function resolve(): void
     {
-
-
-        $path = strtolower($this->request->getPath());
         $method = strtolower($this->request->getMethod());
+        $path = $this->clean($this->request->getPath());
 
-        $callback = $this->routes[$method][$path];
+        $callback = $this->routes[$method][$path] ?? null;
+
+        if (!$callback) {
+            http_response_code(404);
+            echo '404 - Route not found';
+            return;
+        }
 
         if (is_string($callback)) {
             echo $this->renderView($callback);
             return;
         }
 
-
         if (is_array($callback)) {
-            $callback[0] = new $callback[0]();
+            $callback[0] = new $callback[0];
         }
 
         call_user_func($callback);
     }
 
+    private function clean(string $path): string
+    {
+        $path = parse_url($path, PHP_URL_PATH);
+
+        $base = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+        if ($base !== '/') {
+            $path = substr($path, strlen($base));
+        }
+
+        $path = '/' . trim($path, '/');
+
+        return $path === '' ? '/' : $path;
+    }
+
+
     public function renderView($view)
     {
-        $target_view = $this->renderOnlyView($view);
+        $content = $this->renderOnlyView($view);
         $layout = $this->renderLayout();
-
-        return str_replace('{{content}}', $target_view, $layout);
+        return str_replace('{{content}}', $content, $layout);
     }
 
     public function renderLayout()
     {
         ob_start();
-        require_once dirname(__dir__) . "/views/layout/layout.php";
+        require dirname(__dir__) . '/views/layout/layout.php';
         return ob_get_clean();
     }
 
     public function renderOnlyView($view)
     {
         ob_start();
-        require_once dirname(__dir__) . "/views/$view.php";
+        require dirname(__dir__) . "/views/$view.php";
         return ob_get_clean();
     }
 }
